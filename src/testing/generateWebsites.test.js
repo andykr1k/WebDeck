ecb814bd1b3e293d8f7a696c11287a860ad48f23
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-12-27 01:45:38 -0800

    Fixing Tests

--- src/testing/generateWebsites.test.js
+++ src/testing/generateWebsites.test.js
@@ -3,3 +3,7 @@
 // test('Creates Simple Website', () => {
 //   expect(generateWebsites("FirstName", "LastName", "Email")).toBeTruthy();
 // });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1+2).toBe(3);
+});
